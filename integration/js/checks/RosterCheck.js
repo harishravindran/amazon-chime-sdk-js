0964a08c35531de26553471efa2dcd520ddcd2f5
Kashish Tayal <katayal@amazon.com>
2020-09-28 12:40:38 -0700

    Increase roster check time for sip test

--- integration/js/checks/RosterCheck.js
+++ integration/js/checks/RosterCheck.js
@@ -3,13 +3,15 @@ const demo = require('../pages/AppPage');
 const AppTestStep = require('../utils/AppTestStep');
 
 class RosterCheck extends AppTestStep {
-  constructor(kiteBaseTest, sessionInfo, numberOfParticipant) {
+
+  constructor(kiteBaseTest, sessionInfo, numberOfParticipant, config = new RosterCheckConfig() ) {
     super(kiteBaseTest, sessionInfo);
     this.numberOfParticipant = numberOfParticipant;
+    this.config = config;
   }
 
-  static async executeStep(KiteBaseTest, sessionInfo, numberOfParticipant) {
-    const step = new RosterCheck(KiteBaseTest, sessionInfo, numberOfParticipant);
+  static async executeStep(KiteBaseTest, sessionInfo, numberOfParticipant, config = new RosterCheckConfig()) {
+    const step = new RosterCheck(KiteBaseTest, sessionInfo, numberOfParticipant, config);
     await step.execute(KiteBaseTest);
   }
 
@@ -26,11 +28,19 @@ class RosterCheck extends AppTestStep {
   }
 
   async run() {
-    const rosterCheckPassed = await this.page.rosterCheck(this.numberOfParticipant);
+    const rosterCheckPassed = await this.page.rosterCheck(this.numberOfParticipant, this.config.checkCount, this.config.waitTimeMs);
     if (!rosterCheckPassed) {
       throw new KiteTestError(Status.FAILED, 'Participants are not present on the roster');
     }
   }
 }
 
-module.exports = RosterCheck;
+class RosterCheckConfig {
+  constructor(checkCount = 10, waitTimeMs = 100){
+    this.checkCount = checkCount;
+    this.waitTimeMs = waitTimeMs;
+  }
+}
+
+module.exports.RosterCheck = RosterCheck;
+module.exports.RosterCheckConfig = RosterCheckConfig;
